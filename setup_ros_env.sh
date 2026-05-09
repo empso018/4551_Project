7a65e38d8deee24d494dd807d1134e8813329cb4
TylerHamilton7 <hammypuck@yahoo.com>
2026-05-09 00:49:38 -0500

    Modify ROS environment setup script
    Updated script to use workspace directory for sourcing ROS and added virtual environment activation.

--- setup_ros_env.sh
+++ setup_ros_env.sh
@@ -1,8 +1,15 @@
+# Workspace directory, assuming that Step 0 (Lab setup/ros2 apt), 
+# Step 1 (Clone/Venv), and Step 2 (Build) are complete.
+WS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Source system ROS
 source /opt/ros/jazzy/setup.bash
 
 # Source workspace
-source ~/4551_Project/install/setup.bash
+source "$WS_DIR/install/setup.bash"
 
-# Add virtualenv packages
-export PYTHONPATH=~/ros2_venv/lib/python3.12/site-packages:$PYTHONPATH
+# Source the venv
+source "$HOME/ros2_venv/bin/activate"
+
+# Need the burger bot
+export TURTLEBOT3_MODEL=burger
